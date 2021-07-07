a9496e4ee8b219e91699dd87c563fe324a5f1617
108EAA0A <k_108e@outlook.jp>
2021-07-07 17:14:34 +0900

    fix symlink permission

--- brew_install.sh
+++ brew_install.sh
@@ -40,5 +40,5 @@ for formula in ${cask_formulas[@]}; do
 done
 
 GCC_VER=$(ls /usr/local/bin | grep -E "^g\+\+\-(\d+) \->" | awk '{print $1}' | sed -e "s/g++-//g")
-ln -snfv /usr/local/bin/gcc-${GCC_VER} /usr/local/bin/gcc
-ln -snfv /usr/local/bin/g++-${GCC_VER} /usr/local/bin/g++
+sudo ln -snfv /usr/local/bin/gcc-${GCC_VER} /usr/local/bin/gcc
+sudo ln -snfv /usr/local/bin/g++-${GCC_VER} /usr/local/bin/g++
