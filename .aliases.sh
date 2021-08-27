09061f672ffb7137a6256887e62453625b460b33
108EAA0A <k_108e@outlook.jp>
2021-08-27 11:25:41 +0900

    add alias

--- .aliases.sh
+++ .aliases.sh
@@ -11,7 +11,8 @@ alias dc='docker compose'
 alias kusa='curl https://github-contributions-api.deno.dev/$(git config user.name).term'
 
 alias gca='git commit --amend'
-alias gcann='git commit --amend --no-edit -n'
+alias gcan='gca --no-edit'
+alias gcann='gcan -n'
 alias gfpo='git push --force-with-lease origin'
 
 alias fixmp4='ffmpeg -c copy -bsf:a aac_adtstoasc'
