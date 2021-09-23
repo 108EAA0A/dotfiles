619ce91de0d5745d1888f4333a25c7f540aa7e2c
108EAA0A <k_108e@outlook.jp>
2021-09-24 02:47:21 +0900

    change zsh completion message

--- .zplug.zsh
+++ .zplug.zsh
@@ -59,3 +59,9 @@ zplug load
 # export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 # eval $(dircolors $ZPLUG_HOME/repos/seebi/dircolors-solarized/dircolors.ansi-dark)
 # zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# 補完メッセージを読みやすくする
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*' format '%B%d%b'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*:warnings' format 'No matches for: %d'
