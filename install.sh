6ed58781d6db5291307535b7590f60335b3fa803
108EAA0A <k_108e@outlook.jp>
2022-03-04 16:24:54 +0900

    skip install vscode extensions

--- install.sh
+++ install.sh
@@ -101,8 +101,8 @@ source "${DOT_DIR}/asdf_install.sh"
 echo "install node modules..."
 npm i -g typescript ts-node create-react-app
 
-echo "install vscode extensions..."
-source "${DOT_DIR}/vscode/extensions.sh"
+# echo "install vscode extensions..."
+# source "${DOT_DIR}/vscode/extensions.sh"
 
 echo "Loading dotfiles..."
 source "${DOT_DIR}/.zshrc"
